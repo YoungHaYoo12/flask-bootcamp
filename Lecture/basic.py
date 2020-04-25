b758f0b7d3bbe21dbff2379615c43eba3c69048d
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-04-25 13:10:28 +0000

    Add InfoForm class

--- Lecture/basic.py
+++ Lecture/basic.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import (StringField, BooleanField, DateTimeField,RadioField,SelectField,TextField,TextAreaField,SubmitField)
+from wtforms.validators import DataRequired 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+
+class InfoForm(FlaskForm):
+  breed = StringField('What breed are you?', validators=[DataRequired()])
+  neutered = BooleanField('Have you been neutered?')
+  mood = RadioField('Please choose your mood:', choices=[('mood_one','Happy'),('mood_two','Excited')])
+  food_choice = SelectField('Pick your favorite food:',choices=[('chi','Chicken'),('bf','Beef'),('fish','Fish')])
+  feedback = TextAreaField()
+  submit = SubmitField('Submit')
+
 
 @app.route('/')
 def index():
