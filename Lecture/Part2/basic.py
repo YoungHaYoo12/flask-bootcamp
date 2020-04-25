59d5d3d2d5ee62a3ec488b0cc78bb1dfac12bff8
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-04-25 13:47:56 +0000

    Add content to index.html

--- Lecture/Part2/basic.py
+++ Lecture/Part2/basic.py
@@ -14,6 +14,7 @@ def index():
   if form.validate_on_submit():
     flash('You just clicked the button!')
     return redirect(url_for('index'))
+    
   return render_template('index.html',form=form)
 
 if __name__ == '__main__':
