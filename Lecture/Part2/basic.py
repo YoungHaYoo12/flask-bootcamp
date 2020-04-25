3805daeb815618cdcf7cfe09b1e9e2441e5d2dbf
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-04-25 13:53:51 +0000

    Add breed to form functionality

--- Lecture/Part2/basic.py
+++ Lecture/Part2/basic.py
@@ -6,15 +6,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
 
 class SimpleForm(FlaskForm):
+  breed = StringField('What breed are you?')
   submit = SubmitField('Click Me')
 
 @app.route('/',methods=['GET','POST'])
 def index():
   form = SimpleForm()
   if form.validate_on_submit():
-    flash('You just clicked the button!')
+    session['breed'] = form.breed.data
+    flash('You just changed your breed to: ')
     return redirect(url_for('index'))
-    
+
   return render_template('index.html',form=form)
 
 if __name__ == '__main__':
