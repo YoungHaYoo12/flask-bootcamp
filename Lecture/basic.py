7d9d37838fcec96c0f6df88f01984d7affcc296e
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-04-25 10:33:52 +0000

    Remove all routes

--- Lecture/basic.py
+++ Lecture/basic.py
@@ -2,17 +2,5 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('basic.html')
-
-@app.route('/puppy/<name>')
-def puppy(name):
-  return "<h1>This is a page for {}</h1>".format(name.upper())
-
-@app.route('/home')
-def home():
-  return render_template('home.html')
-
 if __name__ == '__main__':
   app.run('0.0.0.0',8080, debug=True)
